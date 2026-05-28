752a493838a6639adf47e026c16b26835d8cece8
MeecM <rileymclinn@gmail.com>
2026-05-27 17:15:28 -0700

    chap2

--- helloworld/django_project/urls.py
+++ helloworld/django_project/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # we add include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("pages.urls")),
+    # ABOVE we add pages.urls. Now when someone visits homepage, empty string, django looks within pages app for URL routes.
 ]
--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,6 +1,9 @@
 # pages/urls.py
 from django.urls import path  # import
 
+# referring to views.py with from.views, thats how we get it here. home_page_view is part of that.
 from .views import home_page_view
 
+# There are two things going on here. The route is defined by empty string.
+# Then a reference to the view home_page_view
 urlpatterns = [path("", home_page_view)]
