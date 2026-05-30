c38dc0a0d9895a80e4617984cd2fbb7f2c114426
MeecM <rileymclinn@gmail.com>
2026-05-30 00:18:36 +0000

    ch4

--- ch4-company/pages/urls.py
+++ ch4-company/pages/urls.py
@@ -1,8 +1,8 @@
-# pages/urls.py
-#
 from django.urls import path
-from .views import home_page_view
+
+from .views import home_page_view, AboutPageView
 
 urlpatterns = [
-    path("", home_page_view),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("", home_page_view, name="home"),
 ]
--- ch4-company/pages/views.py
+++ ch4-company/pages/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView  # new added
 
 
 def home_page_view(request):
-    return render(request, "home.html")
+    context = {
+        "inventory_list": ["Wiget 1", "Widget 2", "Widget 3"],
+        "greeting": "THank you FOR VisitING",
+    }
+    return render(request, "home.html", context)
 
 
-# Create your views here.
+class AboutPageView(TemplateView):  # new
+    template_name = "about.html"
+
+    def get_context_data(self, **kwags):
+        context = super().get_context_data(**kwags)
+        context["contact_address"] = "123 main"
+        context["phone_number"] = "555-555-5555"
+        return context
